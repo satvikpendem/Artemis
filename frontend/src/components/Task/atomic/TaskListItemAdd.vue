96c81ba178ce4eb722fa0f013a42184a585c5617
Satvik Pendem <satvik.pendem@gmail.com>
2019-02-06 19:21:29 -0500

    Add global button component, utilize in TaskListItemAdd

--- frontend/src/components/Task/atomic/TaskListItemAdd.vue
+++ frontend/src/components/Task/atomic/TaskListItemAdd.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="task-list-item-add">
     <!-- For adding tasks, use <form @submit.prevent="handleSubmit"> -->
-    <form @submit.prevent="addTask">
+    <form id="task-list-item-add-form" @submit.prevent="addTask">
       <input id="title-input" type="text" placeholder="Title" v-model="newTaskTitle">
       <input
         id="duration-input"
@@ -9,13 +9,13 @@
         placeholder="Duration (ex. 8:00, 15m, 4h30)"
         v-model="newTaskDuration"
       >
-      <button type="submit">Submit</button>
+      <AtomicButton id="task-add-button" type="submit">Submit</AtomicButton>
     </form>
   </div>
 </template>
 
 <script>
-// import TaskButton from "@/components/Task/atomic/TaskButton";
+import AtomicButton from "@/components/_global/_atomic/AtomicButton.vue";
 
 export default {
   name: "TaskListItemAdd",
@@ -24,6 +24,9 @@ export default {
       newTaskTitle: "",
       newTaskDuration: ""
     };
+  },
+  components: {
+    AtomicButton
   },
   methods: {
     addTask() {
@@ -40,4 +43,33 @@ export default {
 </script>
 
 <style scoped>
+#task-list-item-add-form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-content: space-between;
+  padding: 1rem 0;
+}
+
+input {
+  outline: none;
+  border: none;
+  background-image: none;
+  background-color: transparent;
+  box-shadow: none;
+  border-bottom: 2.5px solid var(--text-color-disabled);
+
+  padding: 2rem, 0;
+  margin-bottom: 10px;
+
+  font-size: 1rem;
+  font-weight: 700;
+
+  transition: 100ms;
+  transition-timing-function: ease-in-out;
+}
+
+input:focus {
+  border-bottom: 5px solid var(--accent-color);
+}
 </style>
---
+++ frontend/src/components/_global/_atomic/AtomicButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button id="atomic-button">
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AtomicButton"
+};
+</script>
+
+<style scoped>
+button {
+  background: none;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  color: white;
+  background-color: var(--accent-color);
+  border-radius: 5px;
+  margin-top: 1rem;
+  padding: 0.5rem;
+}
+</style>
